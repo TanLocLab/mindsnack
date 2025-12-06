7b99cc713a3e57cea4a469dd159d562c9a2b304c
Tan Loc <locle363@gmail.com>
2025-12-06 18:45:00 +0700

    fix build

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,30 +1,29 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       typography: {
         DEFAULT: {
           css: {
-            maxWidth: 'none',
-            color: '#374151',
-            lineHeight: '1.75',
-            fontSize: '1rem',
-            '> *': {
-              marginTop: '1.25em',
-              marginBottom: '1.25em',
+            maxWidth: "none",
+            color: "#374151",
+            lineHeight: "1.75",
+            fontSize: "1rem",
+            "> *": {
+              marginTop: "1.25em",
+              marginBottom: "1.25em",
             },
           },
         },
       },
     },
   },
-  plugins: [require('@tailwindcss/typography')],
-}
-export default config
-
+  plugins: [],
+};
+export default config;
